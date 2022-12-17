8a38dbd6f31022d5d838071111219ba9e4ff8ee2
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2022-12-16 23:37:32 -0500

    Added initial code
    Begin project

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+//* Get HTML source files for inclusion
+function includeHTML() {
+    let z, i, elmnt, file, xhttp;
+    // Loop through a collection of all HTML elements:
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        // Search for elements with a certain atrribute:
+        file = elmnt.getAttribute("include");
+        if (file) {
+            // Make an HTTP request using the attribute value as the file name:
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    // Remove the attribute, and call this function once more:
+                    elmnt.removeAttribute("include");
+                    includeHTML();
+                };
+            };
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            // Exit the function:
+            return;
+        };
+    };
+};
+
+// Load include files into index.html
+includeHTML();
+
+$(() => {
+    // Set image base file path
+    $("img")
+        .attr("src", function (index, src) {
+            return "media/" + src;
+        });
+});
+
+console.log("Ran script.js");
