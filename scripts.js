e90484331e5ffe34be56b1d00a05666f66a36c11
JonVojtush <jonathanvojtush@gmail.com>
2023-01-22 20:53:10 -0500

    modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -3,65 +3,55 @@
 let siteTitle = "jsFrame",
     main = document.querySelector("main");
 
-function includeHTML() {  // https://www.w3schools.com/howto/howto_html_include.asp
-// Use the include attribute to load html content dynamically into an html framework
+function includeHTML() {  // Load data into the index.html from other source files. https://www.w3schools.com/howto/howto_html_include.asp
     let z, i, elmnt, include, xhttp;
-    // Loop through a collection of specific HTML elements:
     z = document.querySelectorAll("header, main, aside, footer");
-    for (i = 0; i < z.length; i++) {
+    for (i = 0; i < z.length; i++) {  // Loop through elements that contain the include attribute
         elmnt = z[i];
-        // Search for elements with a certain atrribute:
         include = elmnt.getAttribute("include");
-        if (include) {
-            // Make an HTTP request using the attribute value as the file name:
+        if (include) {  // Search for elements with the include atrribute
             xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+            xhttp.onreadystatechange = function() {  // Make an HTTP request using the include attribute value as the file name to fetch data from
                 if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}  // Update elmnt with the include file's data
                     if (this.status == 404) {elmnt.innerHTML = "Content not found. Please let us know what is broken so we can fix it. Thank you.";}
-                    // Remove the attribute, and call this function once more:
-                    elmnt.removeAttribute("include");
-                    includeHTML();
+                    elmnt.removeAttribute("include");  //? Why is this necessary?
+                    includeHTML();  //? Why is this necessary?
                 };
             };
             xhttp.open("GET", include, true);
             xhttp.send();
-            // Exit the function:
             return;
         };
     };
 };
 
-includeHTML();  // Load include files into index.html
-
-function setLinks() {
+function setLinks() {  // Disable default behaviour for <a> tags and configure them to update the content area
     let z, i, elmnt, href, xhttp, link;
-    // Loop through a collection of specific HTML elements:
     z = document.querySelectorAll("a");
-            console.log(z.length + " <a> tag(s) found");  // BUG : This is only fetching 1 a element when it should find atleast 5
-    for (i = 0; i < z.length; i++) {
+            console.log(z.length + " <a> tag(s) found");  //! Not finding any of the <a> tags brought in from header file when it should find atleast 5
+    for (i = 0; i < z.length; i++) {  // Loop through a collection of <a> tags
         elmnt = z[i];
         link = elmnt.getAttribute("target");
         if (link !== "_blank") {
             elmnt.className += " disabled";
             addEventListener(elmnt, "click", function() {
                 href = elmnt.getAttribute("href");
-                // Make an HTTP request using the attribute value as the file name:
                 xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function(href) {
+                xhttp.onreadystatechange = function(href) {  // Make an HTTP request using the href attribute value as the file name to fetch from
                     if (this.readyState == 4) {
-                        if (this.status == 200) {main.innerHTML = this.responseText;} // Update the main content area
+                        if (this.status == 200) {main.innerHTML = this.responseText;}  // Update the main content area
                         if (this.status == 404) {main.innerHTML = "Content not found. Please let us know what is broken so we can fix it. Thank you.";}
                         return;
                     };
                 };
                 xhttp.open("GET", href, true);
                 xhttp.send();
-                // Exit the function:
                 return;
             });
         };
     };
 };
 
+includeHTML();
 setLinks();
